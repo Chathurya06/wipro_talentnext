8e0c09e3fe47ecf2ebbf80be29607fe7c222ac78
Chathurya <chathurya.66@gmail.com>
2025-07-07 11:31:27 +0530

    javafundamentals

---
+++ java_fundamentals/src/java_fundamentals/ArraysQ3.java
@@ -0,0 +1,28 @@
+package java_fundamentals;
+import java.util.Scanner;
+
+public class ArraysQ3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int[] arr=new int[n];
+		
+		for(int i=0;i<n;i++) {
+			arr[i]=sc.nextInt();
+		}
+		int search=sc.nextInt();
+		for(int i=0;i<n;i++) {
+			if(arr[i]==search) {
+				System.out.print(i);
+				return;
+			}
+			
+			}
+		System.out.println(-1);
+
+
+	}
+
+}
